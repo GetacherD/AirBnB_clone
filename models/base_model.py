40988eb2457acad43118a00262ce19eee590eee7
GetacherD <getacher.d@gmail.com>
2022-08-03 10:30:24 +0300

    save changes

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -25,7 +24,7 @@ class BaseModel:
                         setattr(self, key, datetime.fromisoformat(value))
                     else:
                         setattr(self, key, value)
-            storage.save()
+        storage.new(self)
 
     def __str__(self):
 
@@ -37,16 +36,16 @@ class BaseModel:
 
         """ save instance to file"""
         setattr(self, "updated_at",  datetime.now())
-        print("type of self.updated ", type(self.updated_at))
         storage.save()
 
     def to_dict(self):
 
         """ Return Dict Representation """
-        dic = self.__dict__
-        print("BEFORE EXCEPTION", dic)
-        print("type of updated is ", type(dic["updated_at"]))
+        dic = {}
+        for key, value in self.__dict__.items():
+            if key in ["updated_at", "created_at"]:
+                dic[key] = value.isoformat()
+            else:
+                dic[key] = value
         dic["__class__"] = self.__class__.__name__
-        dic["updated_at"] = self.updated_at.isoformat()
-        dic["created_at"] = self.created_at.isoformat()
         return dic
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,20 @@
 File Storage Module
 """
 import json
+from json import JSONEncoder
+from datetime import datetime
+from os.path import exists
+
+
+class DateTimeEncoder(JSONEncoder):
+
+    """ DateTimeEncoder to encode datetime objecsts to json"""
+    def default(self, o):
+
+        """ Default encoding """
+        if isinstance(o, (datetime, datetime.date)):
+            return o.isoformat()
+        return super().default(o)
 
 
 class FileStorage:
@@ -26,12 +40,12 @@ class FileStorage:
 
         """ save objects dictionary to json file"""
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            f.write(json.dumps(FileStorage.__objects))
+            f.write(json.dumps(FileStorage.__objects, cls=DateTimeEncoder))
 
     def reload(self):
 
         """ Deserialize, load objects from json"""
-        if FileStorage.__file_path:
+        if exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
                 result = f.read()
                 if result:
