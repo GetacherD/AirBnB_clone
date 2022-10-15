7152bc38c0e5dd8f44cb2b85dbb78ddeaf70b121
GetacherD <getacher.d@gmail.com>
2022-10-15 23:03:26 +0300

    test

--- models/base_model.py
+++ models/base_model.py
@@ -11,6 +11,7 @@ class BaseModel():
     """ Base Model for all """
     def __init__(self, *args, **kwargs):
         """ Initialize new object """
+        args = None
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
