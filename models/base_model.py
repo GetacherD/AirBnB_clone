889e8e6845273ffd5d60b89011d3b9936213c1c8
GetacherD <getacher.d@gmail.com>
2022-10-15 21:29:37 +0300

    upadte recreation of model

--- models/base_model.py
+++ models/base_model.py
@@ -13,12 +13,13 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        for key, value in kwargs.items():
-            if key != "__class__":
-                if key in ["created_at", "updated_at"]:
-                    setattr(self, key, datetime.fromisoformat(value))
-                else:
-                    setattr(self, key, value)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
 
     def __str__(self):
         """ str representation """
