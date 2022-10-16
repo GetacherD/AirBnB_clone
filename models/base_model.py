6ab3db1ceab03725d61274a2018872b43671746e
GetacherD <getacher.d@gmail.com>
2022-10-16 21:29:01 +0300

    fix style

--- models/base_model.py
+++ models/base_model.py
@@ -32,8 +32,8 @@ class BaseModel:
     def __str__(self):
 
         """ String Representation """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                       self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
 
